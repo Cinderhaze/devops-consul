d7a401e7764ff486cab3505831e8adefd239d004
Daryl Wiest <daryl.wiest@gmail.com>
2016-01-14 01:04:02 -0500

    Updated manifest to move ui to first VM, set mode on script

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -20,6 +20,15 @@ node 'host-1' {
       'client_addr'      => '0.0.0.0',
     }
   }
+  ::consul::service { 'web_ui':
+    checks => [{
+        script   => 'curl -I 127.0.0.1:8500/ui/',
+        interval => '10s'
+      }
+      ],
+    port   => 8500,
+    tags   => ['web']
+  }
 }
 
 node 'host-2' {
@@ -68,51 +77,27 @@ node 'host-4' {
     pretty_config => true,
     config_hash => {
       'data_dir'   => '/opt/consul',
-      'client_addr'      => '0.0.0.0',
       'datacenter' => 'laptop',
       'log_level'  => 'INFO',
       'enable_syslog' => true,
 #      'node_name'  => 'agent',
       'retry_join' => ['172.16.32.10','172.16.32.11','172.16.32.12' ],
-      'ui_dir'           => '/opt/consul/ui',
     }
   }
 
-  ::consul::service { 'web_ui':
-    checks => [{
-        script   => 'curl -I 127.0.0.1:8500/ui/',
-        interval => '10s'
-      }
-      ],
-    port   => 8500,
-    tags   => ['web']
-  }
 }
 
 class consul_check::puppet {
-  $puppet_mon_script = 'https://raw.githubusercontent.com/ripienaar/monitoring-scripts/master/puppet/check_puppet.rb'
+  # script from https://raw.githubusercontent.com/ripienaar/monitoring-scripts/master/puppet/check_puppet.rb
   
-  exec { 'fetch puppet monitoring script':
-    cwd     => '/usr/local/bin',
-    command => "wget $puppet_mon_script",
-    path    => ['/usr/bin'],
-    creates => '/usr/local/bin/check_puppet.rb',
-  } ->
-  file { '/usr/local/bin/check_puppet.rb':
-    mode  => '755',
-    alias => 'check_puppet',
-  }
-
   ::consul::check { 'check_puppet_resources':
     interval => '30s',
-    script   => '/usr/local/bin/check_puppet.rb -f -c 1 -w 1',
-    require  => File['check_puppet'],
+    script   => '/vagrant/scripts/check_puppet.rb -f -c 1 -w 1',
   }
 
   ::consul::check { 'check_puppet_time':
     interval => '30s',
-    script   => '/usr/local/bin/check_puppet.rb -c 360 -w 360',
-    require  => File['check_puppet'],
+    script   => '/vagrant/scripts/check_puppet.rb -c 360 -w 360',
   }
 }
 
